7418ce041c1a5cd40e32e553cbaac51cc9379081
Akshat1809 <ranjan.akshat2007@gmail.com>
2021-04-09 19:15:21 +0530

    eee

---
+++ index.js
@@ -0,0 +1,19 @@
+app.listen(3000, () => {
+    console.log('Bot Is Online!');
+  });
+  
+  const Discord = require("discord.js");
+  const client = new Discord.Client();
+  const mongoose = require('mongoose');
+  
+  mongoose.connect('mongodb+srv://Majik:majikisebik@popcat.tsgll.mongodb.net/Data' , {useNewUrlParser : true , useUnifiedTopology : true})
+  
+  require("./ExtendedMessage");
+  client.commands = new Discord.Collection();
+  client.events = new Discord.Collection();
+  
+  ['command_handler', 'event_handler'].forEach(handler => {
+    require(`./handlers/${handler}`)(client, Discord);
+  })
+  
+  client.login(token);//aaaaaaaaaaaaaaaaaa e e  g
